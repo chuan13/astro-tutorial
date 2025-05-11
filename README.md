1f85939d80fe04f632b2a16ed1584991f93eecf2
Chuan <179152971+chuan13@users.noreply.github.com>
2025-05-11 17:03:55 +0800

    chore: 部署到 GitHub Pages
    fix: 由於設定 base，一併修正相對路徑

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+跟著 Astro docs 的 tutorial 建立的 Astro project。
+
 GitHub Pages 部署於：https://chuan13.github.io/astro-tutorial/
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // deploy to Github Pages
+    site: 'https://chuan13.github.io',
+    base: '/astro-tutorial'
+});
